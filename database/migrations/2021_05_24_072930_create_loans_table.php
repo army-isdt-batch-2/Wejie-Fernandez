de4994f92e6db0e0bc81793b8e854b7080f0e2e6
Wejie-hub <rogierodriguez3@gmail.com>
2021-05-24 23:56:54 -0700

    most updated1

--- database/migrations/2021_05_24_072930_create_loans_table.php
+++ database/migrations/2021_05_24_072930_create_loans_table.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Database\Migrtion\Migration;
+use Illuminate\Database\Migration\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
